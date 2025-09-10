6c19ce219446ed679f7a678fc1a0f08207406448
Dipak Mehata <dipakmehta9804@gmail.com>
2025-09-10 12:54:59 +0530

    basic operation in 2D Array

---
+++ Multidimensional_Arrays/Sum_ofAll_element_in2Darray.java
@@ -0,0 +1,25 @@
+/*
+    Question:-
+        Write a Java program to add all element of
+        given 2D array[] of integers
+*/
+
+package Multidimensional_Arrays;
+
+public class Sum_ofAll_element_in2Darray {
+    public static void main(String[] args) {
+        int[][] arr = {{1,5,6},{2,-9,3},{5,10,3}};
+        int sum =0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j <arr[0].length; j++) {
+                sum += arr[i][j];
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+
+
+}

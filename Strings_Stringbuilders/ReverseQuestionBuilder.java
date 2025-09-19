1eedab1ecd97f8c114a97e11592116f11055f83e
Dipak Mehata <dipakmehta9804@gmail.com>
2025-09-19 07:51:31 +0530

    Reverser withiout reverse method

--- Strings_Stringbuilders/ReverseQuestionBuilder.java
+++ Strings_Stringbuilders/ReverseQuestionBuilder.java
@@ -29,12 +29,19 @@ public class ReverseQuestionBuilder {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         String s = sc.nextLine();
-
         StringBuilder sb = new StringBuilder(s);
-
-        sb.reverse(sb, 2, 6);
+        int n = sb.length();
+        int i=0; int j= 0;
+        while(j < n){
+            if(sb.charAt(j) != ' ') j++;
+            else {
+                reverse(sb, i,j-1);
+                i = j+1;
+                j = i;
+            }
+        }
+        reverse(sb, i,j-1);
         System.out.println(sb);
         sc.close();
     }
